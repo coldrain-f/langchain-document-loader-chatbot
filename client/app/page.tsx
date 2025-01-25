0e92b53a9fa5c36b9cc62b55ae99a87572f5eda0
coldrain-f <intmain__@naver.com>
2025-01-25 16:06:42 +0900

    ChatBot 임시 MenuBar UI 추가

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -22,6 +22,22 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
 interface Message {
   role: "user" | "assistant";
   content: string;
@@ -61,6 +77,9 @@ const Home = () => {
   const [message, setMessage] = useState<Message>();
   const [documents, setDocuments] = useState<Document[]>([]);
   const [isDone, setIsDone] = useState(true);
+
+  // 파일 이름 배열
+  const [fileNames, setFileNames] = useState([]);
   const scrollRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -100,9 +119,98 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex justify-center gap-5 ms-5 mr-5 mt-5">
+      <div className="flex justify-center gap-5 mx-auto mt-5">
         <div>
           <Card className="flex flex-col w-[700px] h-[700px]">
+            <Menubar className="border-t-0 border-s-0 border-r-0">
+              <MenubarMenu>
+                <MenubarTrigger>File</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>
+                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem>
+                    New Window <MenubarShortcut>⌘N</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem disabled>New Incognito Window</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarSub>
+                    <MenubarSubTrigger>Share</MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>Email link</MenubarItem>
+                      <MenubarItem>Messages</MenubarItem>
+                      <MenubarItem>Notes</MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                  <MenubarSeparator />
+                  <MenubarItem>
+                    Print... <MenubarShortcut>⌘P</MenubarShortcut>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger>Edit</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>
+                    Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem>
+                    Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarSub>
+                    <MenubarSubTrigger>Find</MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>Search the web</MenubarItem>
+                      <MenubarSeparator />
+                      <MenubarItem>Find...</MenubarItem>
+                      <MenubarItem>Find Next</MenubarItem>
+                      <MenubarItem>Find Previous</MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                  <MenubarSeparator />
+                  <MenubarItem>Cut</MenubarItem>
+                  <MenubarItem>Copy</MenubarItem>
+                  <MenubarItem>Paste</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger>View</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarCheckboxItem>
+                    Always Show Bookmarks Bar
+                  </MenubarCheckboxItem>
+                  <MenubarCheckboxItem checked>
+                    Always Show Full URLs
+                  </MenubarCheckboxItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>
+                    Reload <MenubarShortcut>⌘R</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem disabled inset>
+                    Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Hide Sidebar</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger>Profiles</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarRadioGroup value="benoit">
+                    <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
+                    <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
+                    <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
+                  </MenubarRadioGroup>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Edit...</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Add Profile...</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
             <CardHeader>
               <CardTitle>ChatBot</CardTitle>
               <CardDescription>
@@ -119,6 +227,7 @@ const Home = () => {
                 </p>
               </CardDescription>
             </CardHeader>
+
             <ScrollArea className="flex-1 p-4">
               <div ref={scrollRef}>
                 {messages.map((message, index) => (

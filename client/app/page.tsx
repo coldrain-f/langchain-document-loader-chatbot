f29a9f5400a8a1d100fd193b998c7e8128a7d550
coldrain-f <intmain__@naver.com>
2025-01-28 03:03:08 +0900

    feat: 임시 MenuBar 추가

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -29,6 +29,22 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Switch } from "@/components/ui/switch";
 
@@ -123,6 +139,95 @@ const Home = () => {
       <div className="flex justify-center">
         <div>
           <Card className="flex flex-col w-[700px] h-[705px] rounded-r-none">
+            <Menubar className="border-s-0 border-t-0 border-r-0 rounded-b-none rounded-r-none">
+              <MenubarMenu>
+                <MenubarTrigger>File</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>
+                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem>
+                    New Window <MenubarShortcut>⌘N</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem disabled>New Incognito Window</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarSub>
+                    <MenubarSubTrigger>Share</MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>Email link</MenubarItem>
+                      <MenubarItem>Messages</MenubarItem>
+                      <MenubarItem>Notes</MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                  <MenubarSeparator />
+                  <MenubarItem>
+                    Print... <MenubarShortcut>⌘P</MenubarShortcut>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger>Edit</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>
+                    Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem>
+                    Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarSub>
+                    <MenubarSubTrigger>Find</MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>Search the web</MenubarItem>
+                      <MenubarSeparator />
+                      <MenubarItem>Find...</MenubarItem>
+                      <MenubarItem>Find Next</MenubarItem>
+                      <MenubarItem>Find Previous</MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                  <MenubarSeparator />
+                  <MenubarItem>Cut</MenubarItem>
+                  <MenubarItem>Copy</MenubarItem>
+                  <MenubarItem>Paste</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger>View</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarCheckboxItem>
+                    Always Show Bookmarks Bar
+                  </MenubarCheckboxItem>
+                  <MenubarCheckboxItem checked>
+                    Always Show Full URLs
+                  </MenubarCheckboxItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>
+                    Reload <MenubarShortcut>⌘R</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem disabled inset>
+                    Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Hide Sidebar</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger>Profiles</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarRadioGroup value="benoit">
+                    <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
+                    <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
+                    <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
+                  </MenubarRadioGroup>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Edit...</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Add Profile...</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
             <CardHeader>
               <CardTitle>AI 도우미</CardTitle>
               <CardDescription>

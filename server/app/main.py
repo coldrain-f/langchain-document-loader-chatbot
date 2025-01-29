8e61422a703464fb2ded06200e7e78a7490399e3
coldrain-f <intmain__@naver.com>
2025-01-30 03:31:54 +0900

    refactor: Python 전체적인 코드 개선

--- server/app/main.py
+++ server/app/main.py
@@ -22,6 +22,8 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.runnables import Runnable
 from langchain.prompts import PromptTemplate
 from langchain.schema.output_parser import StrOutputParser
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough
 
 # CORS 미들웨어 Import
 from fastapi.middleware.cors import CORSMiddleware
@@ -39,25 +41,6 @@ app.add_middleware(
 
 
 # ┌───────────────── Function ───────────────────────────
-# PDF 파일을 List[Document]로 변환
-def read_pdf(file_path: str) -> List[Document]:
-    response = []
-    loader = PyMuPDFLoader(file_path)
-    documents = loader.load()
-    for document in documents:
-        document.metadata["file_path"] = file_path
-
-    response.extend(documents)
-    return response
-
-
-# List[Document]를 벡터 DB에 저장
-def save_to_vector_store(documents: List[Document]) -> None:
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    vector_store = FAISS.from_documents(documents, embedding=embeddings)
-    vector_store.save_local("faiss_index")
-
-
 def convert_pdf_to_images(pdf_path: str, dpi: int = 250) -> List[str]:
     doc = fitz.open(pdf_path)  # 문서 열기
     image_paths = []
@@ -87,43 +70,6 @@ def natural_sort_key(s):
     return [int(text) if text.isdigit() else text for text in re.split(r"(\d+)", s)]
 
 
-# 마크다운 문서로 참고 문서 요약 정리
-def convertMarkdown() -> Runnable:
-    template = """
-    다음의 문서를 가지고 마크다운(Markdown)으로 간단하게 요약 정리해서 작성해줘
-    - 마크다운 Table은 만들지 말아줘
-    - 곧바로 응답결과를 말해줘
-    - 제목: 참고 문서 정리
-        문서: {document}
-    """
-
-    custom_rag_prompt = PromptTemplate.from_template(template)
-    model = ChatOpenAI(model="gpt-4o-mini")
-
-    return custom_rag_prompt | model | StrOutputParser()
-
-
-# RAG Chain
-def get_rag_chain() -> Runnable:
-    template = """
-    다음의 컨텍스트를 활용해서 질문에 답변해줘
-    - 질문에 대한 응답을 해줘
-    - 간결하게 5줄 이내로 해줘
-    - 곧바로 응답결과를 말해줘
-    - 너의 이름은 주택청약 FAQ 도우미야
-
-    컨텍스트 : {context}
-
-    질문: {question}
-
-    응답:"""
-
-    custom_rag_prompt = PromptTemplate.from_template(template)
-    model = ChatOpenAI(model="gpt-4o-mini")
-
-    return custom_rag_prompt | model | StrOutputParser()
-
-
 # 사용자 질문에 대한 RAG 처리
 def process_question(user_question: str, include_additional_info: bool):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
@@ -137,21 +83,53 @@ def process_question(user_question: str, include_additional_info: bool):
     retriever = new_db.as_retriever(search_kwargs={"k": 3})
 
     ## 사용자 질문을 기반으로 관련문서 3개 검색
-    retrieve_docs: List[Document] = retriever.invoke(user_question)
+    retriever_docs = retriever.invoke(user_question)
 
-    ## RAG 체인 선언
-    chain = get_rag_chain()
+    # 프롬포트
+    prompt = PromptTemplate.from_template(
+        """You are an assistant for question-answering tasks.
+    Use the following pieces of retrieved context to answer the question.
+    If you don't know the answer, just say that you don't know.
+    Answer in Korean.
+
+    #Question : {question}
+
+    #Context: {context}
+    
+    #Answer:"""
+    )
+
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | model
+        | StrOutputParser()
+    )
 
     ## 질문과 문맥을 넣어서 체인 결과 호출
-    response = chain.invoke({"question": user_question, "context": retrieve_docs})
+    response = chain.invoke(user_question)
 
     ## 마크다운으로 변환
     if include_additional_info:
-        chain = convertMarkdown()
-        markdownResponse = chain.invoke({"document": retrieve_docs})
-        return response, markdownResponse, retrieve_docs
+        markdown_prompt = PromptTemplate.from_template(
+            """Use the following document to write a summary Markdown in Korean.
+            Don't create a Markdown Table.
+            Title: 참고 문서 정리
+            #Document: {document}"""
+        )
+        markdown_model = ChatOpenAI(model="gpt-4o")
+        markdown_chain = (
+            {"document": RunnablePassthrough()}
+            | markdown_prompt
+            | markdown_model
+            | StrOutputParser()
+        )
+        markdown_response = markdown_chain.invoke(retriever_docs)
+        return response, markdown_response, retriever_docs
 
-    return response, "", retrieve_docs
+    return response, "", retriever_docs
 
 
 # ┌───────────────── DTO ───────────────────────────
@@ -163,7 +141,6 @@ class ChatbotCompletionsRequest(BaseModel):
 # ┌───────────────── API ───────────────────────────
 @app.post("/api/v1/chatbot/completions")
 def completions(request: ChatbotCompletionsRequest):
-    print(request.question)
     # 사용자 질문 처리
     response, markdownResponse, context = process_question(
         request.question, request.include_additional_info
@@ -201,9 +178,26 @@ def completions(request: ChatbotCompletionsRequest):
     }
 
 
+# RAG 전처리 함수
+def build_knowledge_base():
+    # 1. Load
+    loader = PyMuPDFLoader("./files/indexed/주택청약_FAQ_202405.pdf")
+    documents = loader.load()
+
+    # 2. Split
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+    split_documents = text_splitter.split_documents(documents)
+
+    # 3. Embed & Store
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    vector_store = FAISS.from_documents(split_documents, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
 @app.get("/")
 def root():
-    return {"message": "Hello World!"}
+    # build_knowledge_base()
+    return {"message": "지식 베이스 구축이 완료되었습니다."}
 
 
 if __name__ == "__main__":
